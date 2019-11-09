f3a9b32f0e5240635353660f578330807585789b
mandrei100 <mcrisandreica@hotmail.com>
2019-11-09 16:34:36 -0500

    WIP

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ app.get('/', (req, res, next)=> {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+app.get('/api/products', (req, res, next)=> {
+    db.findAll()
+    .then(data => Response.send(data))
+    .catch(e => console.log('get products error', el));
+  //  res.send(products);
+});
+
 app.listen(3000, ()=> console.log('listening on port 3000'));
